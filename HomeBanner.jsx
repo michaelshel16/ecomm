ff83770fc973c2ad633a926df22bb18352effcec
michaelshel16 <113286433+michaelshel16@users.noreply.github.com>
2023-04-19 17:23:28 +0530

    Add files via upload

---
+++ HomeBanner.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./HomeBanner.css";
+import Carousel from 'react-bootstrap/Carousel';
+import HomeBanner1 from "../../assets/img/HomeBanner1.jpg"
+import HomeBanner2 from "../../assets/img/HomeBanner2.jpg"
+import HomeBanner3 from "../../assets/img/HomeBanner3.jpg"
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={HomeBanner1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={HomeBanner2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={HomeBanner3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
+    
