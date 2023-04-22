ff3db2ff47b7edc5f23c65b817993a4a971f58a0
michaelshel16 <113286433+michaelshel16@users.noreply.github.com>
2023-04-23 01:02:16 +0530

    Update HomeBanner.jsx

--- src/components/molecules/HomeBanner.jsx
+++ src/components/molecules/HomeBanner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./HomeBanner.css";
 import Carousel from 'react-bootstrap/Carousel';
-import HomeBanner1 from "../../assets/img/HomeBanner1.jpg"
-import HomeBanner2 from "../../assets/img/HomeBanner2.jpg"
-import HomeBanner3 from "../../assets/img/HomeBanner3.jpg"
+import HomeBanner1 from "../../assets/img/Homebanner1.jpg"
+import HomeBanner2 from "../../assets/img/Homebanner2.jpg"
+import HomeBanner3 from "../../assets/img/Homebanner3.jpg"
 function HomeBanner() {
   return (
     <Carousel>
